39d5d3903055277974851081ec826b8803e011b9
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-11-22 02:08:13 -0600

    adding db connection and dotenv

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-app.get("*", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(PORT, () =>
   console.log(`Server started on http://localhost:${PORT}`)
