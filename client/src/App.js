de9bbe3080ca0992340e40cc4a4280210cbc27d5
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2020-11-22 11:28:53 -0600

    init client, bootswatch and favicon

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "bootswatch/dist/spacelab/bootstrap.min.css";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>IoT Electrocontrol</h1>
+    </div>
+  );
+};
+
+export default App;
